1f76cb2c5121db9da737771a6e6df00ec856b093
Lucas Braga <lucasbraga_a@outlook.com>
2022-06-06 20:06:13 -0300

    apagando console log

--- js/slide.js
+++ js/slide.js
@@ -155,7 +155,7 @@ export class Slide {
   }
 }
 
-export class SlideNav extends Slide {
+export default class SlideNav extends Slide {
   constructor(slide, wrapper) {
     super(slide, wrapper);
     this.bindCrontrolEvents();
